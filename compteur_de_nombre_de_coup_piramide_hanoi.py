253c7043fe426042e415fa24088ad5b8bdc7014e
arthur227 <44922990+arthur227@users.noreply.github.com>
2018-12-23 23:38:00 +0100

    Update compteur_de_nombre_de_coup_piramide_hanoi.py

--- compteur_de_nombre_de_coup_piramide_hanoi.py
+++ compteur_de_nombre_de_coup_piramide_hanoi.py
@@ -1,6 +1,7 @@
-t = 1
+compteur_egal = 16 #Variable a changer selon ce que l'on veut
+t = 1 
 compteur = 1
-while compteur != 16:
+while compteur != compteur_egal:
     t = (2*t)+1
     compteur +=1
     print("Le compteur est égal a " + str(compteur) + ", t est égal à " + str(t))
