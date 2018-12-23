ec47081183694098d685131a73f1fb7a7b18cfb7
arthur227 <44922990+arthur227@users.noreply.github.com>
2018-12-23 23:44:34 +0100

    Update compteur_de_nombre_de_coup_piramide_hanoi.py

--- compteur_de_nombre_de_coup_piramide_hanoi.py
+++ compteur_de_nombre_de_coup_piramide_hanoi.py
@@ -1,7 +1,8 @@
 nb_disque_max = 16 #Changer la variable selon ce que l'on veut  comme nombre max de disques
-t = 1 
+t = 1 #Nombre de coup minimal
 nb_disque = 1
+print("Pour " + str(nb_disque) + " disque(s)" + ", le nombre de coups minimum est égal à " + str(t))
 while nb_disque != nb_disque_max:
     t = (2*t)+1
     nb_disque +=1
-    print("Pour " + str(compteur) + " disque(s)" + ", le nombre de coup minimum est égal à " + str(t))
+    print("Pour " + str(nb_disque) + " disque(s)" + ", le nombre de coups minimum est égal à " + str(t))
